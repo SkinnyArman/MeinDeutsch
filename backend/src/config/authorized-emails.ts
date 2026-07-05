f1b639f51037bac58c32e90bf214d8e40454cf7c
Arman Amini <armanwithamini@gmail.com>
2026-07-05 02:59:44 -0700

    feat: increase whitelist

--- backend/src/config/authorized-emails.ts
+++ backend/src/config/authorized-emails.ts
@@ -1,5 +1,7 @@
 export const AUTHORIZED_EMAILS = [
-  "armanwithamini@gmail.com"
+  "armanwithamini@gmail.com",
+  "dfeiznia@gmail.com",
+  "dibasixx@gmail.com"
 ] as const;
 
 export const authorizedEmailSet = new Set(AUTHORIZED_EMAILS.map((email) => email.toLowerCase()));
